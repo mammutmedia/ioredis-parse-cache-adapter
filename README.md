1133061e816d36f8ec797983b24680847f3290a9
Felix Nithammer <f.nithammer@mammutmedia.eu>
2022-11-27 19:16:28 +0100

    feat: add redis cluster support

--- README.md
+++ README.md
@@ -3,13 +3,16 @@
 parse-server cache-adapter for ioredis
 
 Base on this file:
-https://github.com/parse-community/parse-server/blob/master/src/Adapters/Cache/RedisCacheAdapter.js
+[RedisCacheAdapter.js](https://github.com/parse-community/parse-server/blob/master/src/Adapters/Cache/RedisCacheAdapter.js)
 
-# Installation
+## Installation
 
 `npm install --save ioredis-parse-cache-adapter`
 
-# Usage
+## Usage
+
+Redis single node or Redis sentinel configuration:
+
 ```js
 const parseServer = new ParseServer({
 
@@ -18,3 +21,26 @@ const parseServer = new ParseServer({
     cacheAdapter: new RedisCacheAdapter({ url: process.env.REDIS_URL });
 });
 ```
+
+Redis Cluster configuration:
+
+```js
+const parseServer = new ParseServer({
+
+    /// Other options
+
+    cacheAdapter: new RedisCacheAdapter({
+      cluster: {
+        nodes: process.env.REDIS_CLUSTER_NODES.split(','),
+        options: {
+          redisOptions: {
+            password: process.env.REDIS_PASSWORD,
+          },
+        }
+      }
+    });
+});
+```
+
+You can pass any option that [ioredis](https://github.com/luin/ioredis) supports.
+You may have a look at their documentation.
--- src/index.js
+++ src/index.js
@@ -1,22 +1,53 @@
 const Redis = require('ioredis')
 const LRU = require('lru-cache')
 
-const DEFAULT_REDIS_TTL = 30 * 1000 // 30 seconds in milliseconds
+/* Setting the default time to live for the cache to 30 seconds. */
+const DEFAULT_REDIS_TTL = 30 * 1000
 
+/**
+ * If the ttl is a number and greater than 0, return true, otherwise return false.
+ */
 const isValidTTL = ttl => typeof ttl === 'number' && ttl > 0
 
+/* Setting the default values for the LRU cache. */
 const defaultLruConfig = {
   max: 1000,
   maxAge: 1000 * 60 /// 1 min
 }
 
+/* It's a wrapper around the ioredis library that provides a cache interface */
 class IORedisCacheAdapter {
+  /**
+   * The constructor function takes in a redisContext, a ttl, and an lruConfig. If the redisContext is not provided, an
+   * error is thrown. If the ttl is not provided, the default ttl is used. If the lruConfig is not provided, the default
+   * lruConfig is used
+   * @param redisContext - This is the Redis connection context. It can be a single Redis instance or a Redis cluster.
+   * @param [ttl] - The time to live for the cache.
+   * @param [lruConfig] - This is the configuration for the LRU cache.
+   */
   constructor(redisContext, ttl = DEFAULT_REDIS_TTL, lruConfig = defaultLruConfig) {
+    if (!redisContext) {
+      throw new Error('Redis context is required')
+    }
+
+    this.client =
+      redisContext.cluster && redisContext.cluster.nodes
+        ? new Redis.Cluster(redisContext.cluster.nodes, redisContext.cluster.options)
+        : new Redis(redisContext)
+
     this.ttl = isValidTTL(ttl) ? ttl : DEFAULT_REDIS_TTL
-    this.client = new Redis(redisContext)
     this.map = new LRU(lruConfig)
   }
 
+  /**
+   * "If there is a promise in the map for the given key, return it. Otherwise, return a resolved promise, then chain the
+   * given function to it, and store it in the map."
+   *
+   * The function is used like this:
+   * @param key - The key to store the promise under.
+   * @param promFunction - A function that returns a promise.
+   * @returns A promise that will be resolved when the promise returned by promFunction is resolved.
+   */
   chainPromise(key, promFunction) {
     let p = this.map.get(key)
     if (!p) {
@@ -28,6 +59,11 @@ class IORedisCacheAdapter {
     return p
   }
 
+  /**
+   * It returns a promise that resolves to the value of the key in the cache, or null if the key doesn't exist
+   * @param key - The key to get the value for
+   * @returns A promise that resolves to the value of the key in the cache.
+   */
   get(key) {
     return this.chainPromise(
       key,
@@ -43,6 +79,13 @@ class IORedisCacheAdapter {
     )
   }
 
+  /**
+   * The function takes in a key, value, and ttl (time to live) and sets the value of the key to the value
+   * @param key - The key to store the value under.
+   * @param value - The value to be stored in the cache.
+   * @param [ttl] - Time to live in milliseconds.
+   * @returns A promise that resolves when the value is set.
+   */
   put(key, value, ttl = this.ttl) {
     value = JSON.stringify(value)
     if (ttl === 0) {
@@ -82,6 +125,11 @@ class IORedisCacheAdapter {
     )
   }
 
+  /**
+   * It returns a promise that resolves when the redis client has deleted the key
+   * @param key - The key to delete
+   * @returns A promise that resolves when the redis client has deleted the key.
+   */
   del(key) {
     return this.chainPromise(
       key,
@@ -94,6 +142,10 @@ class IORedisCacheAdapter {
     )
   }
 
+  /**
+   * It returns a promise that resolves when the Redis database is cleared
+   * @returns A promise that resolves when the database is cleared.
+   */
   clear() {
     return new Promise(resolve => {
       this.client.flushdb(function () {
@@ -102,6 +154,10 @@ class IORedisCacheAdapter {
     })
   }
 
+  /**
+   * It returns a promise that resolves to an array of all the keys in the Redis database
+   * @returns A promise that resolves to an array of all the keys in the redis database.
+   */
   // Used for testing
   async getAllKeys() {
     return new Promise((resolve, reject) => {
